e9faadb34e3cf15041418a6ce2e2cb4b6075b6a1
33sakib33 <bsse1108@iit.du.ac.bd>
2022-02-17 13:20:13 +0600

    All 3 organized into separate folders

--- java/src/main/java/workshop/Trivia/TriviaGame.java
+++ java/src/main/java/workshop/Trivia/TriviaGame.java
@@ -31,23 +31,23 @@ public class TriviaGame {
     public boolean add(String playerName) {
         Player newPlayer= new Player(playerName);
         playerList.add(newPlayer);
-        announce(newPlayer.getName() + " was added");
-        announce("They are player number " + playerList.size());
+        System.out.println(newPlayer.getName() + " was added");
+        System.out.println("They are player number " + playerList.size());
         return true;
     }
 
     public void roll(int roll) {
         it=it.hasNext()?it:playerList.iterator();
         currentPlayer=it.next();
-        announce(currentPlayer.getName() + " is the current player");
-        announce("They have rolled a " + roll);
+        System.out.println(currentPlayer.getName() + " is the current player");
+        System.out.println("They have rolled a " + roll);
         currentPlayer.move(roll);
         if(!currentPlayer.isPenaltyBox()) {
             QuestionType currQuesType=null;
             for(QuestionType quesTypeItr: questionList.keySet()){
                 if(quesTypeItr.matchCategory(currentPlayer.getPlace())){
                     currQuesType=quesTypeItr;
-                    announce("The category is " + currQuesType.retType());
+                    System.out.println("The category is " + currQuesType.retType());
                     askQuestion(currQuesType);
                     break;
                 }
@@ -56,28 +56,25 @@ public class TriviaGame {
     }
 
     private void askQuestion(QuestionType curQuesType) {
-        announce(questionList.get(curQuesType.retType()).removeFirst());
+        System.out.println(questionList.get(curQuesType.retType()).removeFirst());
     }
 
     public boolean wasCorrectlyAnswered() {
         if(!getCurrentPlayer().isPenaltyBox()){
-            announce("Answer was correct!!!!");
+            System.out.println("Answer was correct!!!!");
             currentPlayer.setPurse(currentPlayer.getPlace()+1);
-            announce(currentPlayer.getName() + " now has " +currentPlayer.getPurse() + " Gold Coins.");
+            System.out.println(currentPlayer.getName() + " now has " +currentPlayer.getPurse() + " Gold Coins.");
             return didPlayerWin();
         }
         return false;
     }
     public boolean wrongAnswer() {
-        announce("Question was incorrectly answered");
-        announce(currentPlayer.getName()+ " was sent to the penalty box");
+        System.out.println("Question was incorrectly answered");
+        System.out.println(currentPlayer.getName()+ " was sent to the penalty box");
         currentPlayer.setPenaltyBox(true);
         return true;
     }
     private boolean didPlayerWin() {
         return !(currentPlayer.getPurse()==6);
-    }
-    protected void announce(Object message) {
-        System.out.println(message);
     }
 }
